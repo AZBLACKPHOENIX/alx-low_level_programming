da5eebb54982f0171f2bff0ad8516679bcc07a34
AZBLACKPHOENIX <unlimitednigami@gmail.com>
2023-08-11 17:32:11 -0700

    print alphabets in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char alph[26] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+for (i = 0; i < 26; i++)
+putchar(alph[i]);
+putchar('\n');
+return (0);
+}
