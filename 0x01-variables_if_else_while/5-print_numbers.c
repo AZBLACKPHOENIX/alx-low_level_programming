245a2f4097a8da88bc0c35f00b22cf7f52438c0d
AZBLACKPHOENIX <unlimitednigami@gmail.com>
2023-08-12 15:52:06 -0700

    prit numbers from 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints single numbers from 10 to 19
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+for (int i = 10; i < 10; i++)
+{
+printf("%d", i);
+}
+putchar("\n");
+return (0);
+}
