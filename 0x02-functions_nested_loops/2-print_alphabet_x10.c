9785b8a6710ceabca4e24158d88c6f226fcb5900
AZBLACKPHOENIX <unlimitednigami@gmail.com>
2023-08-15 15:59:38 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,18 @@
 #include "main.h"
+
 /**
- * print_alphabet_x10
+ * print_alphabet_x10 - Prints the alphabet 10 times
  */
 void print_alphabet_x10(void)
 {
 int i;
 char ltr;
-for (i = 10; i < 10; i++)
-for (ltr = 'a'; c<= 'z'; c++)
-_putchar(c);
-_putchar('\n');
+for (i = 0; i < 10; i++)
+{
+for (ltr = 'a'; ltr <= 'z'; ltr++)
+{
+ _putchar(ltr)
+}
+ _putchar('\n');
+}
 }
