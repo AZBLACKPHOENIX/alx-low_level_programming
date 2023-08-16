5966df364e0fe79ed9402d1f8315229f07dc6fe0
AZBLACKPHOENIX <unlimitednigami@gmail.com>
2023-08-15 23:37:27 -0700

    Check if character is alphabet

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,19 @@
 #include "main.h"
 
 /**
- * _isalpha - Check if a character is an alphabetic letter
- * @c: The character used to check
+ * _isalpha - Check if a character is an alphabetic letter.
+ * @c: The character used to check.
  *
- * Return: 1 if the character is a letter, 0 otherwise
+ * Return: 1 if the character is a letter, 0 otherwise.
  */
 int _isalpha(int c)
 {
-if ((c >= 'a' && c <= 'z' || (c >= 'A' && c <= 'Z'))
-return (1);
-else
-return (0);
+char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int i;    
+for (i = 0; alphabet[i] != '\0'; i++)
+{
+if (c == alphabet[i])
+return 1;
+}    
+return 0;
 }
