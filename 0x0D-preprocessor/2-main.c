47f98eeea26581097eefd7b743bfd3d7bd95d227
AZBLACKPHOENIX <unlimitednigami@gmail.com>
2023-09-11 10:43:54 -0700

    function like macro

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,11 @@
 /*A Program that prints the name of the file it was compiled from*/
 
 #include <stdio.h>
+#include <unistd.h>
 
 int main()
 {
-printf(__FILE__);
-printf("\n");
+_putchar(__FILE__);
+_putchar("\n");
 return 0;
 }
