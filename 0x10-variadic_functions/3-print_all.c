6fff5e7df1927be56a58c84dec7330abee503528
AZBLACKPHOENIX <unlimitednigami@gmail.com>
2023-09-14 19:59:51 -0700

    a function that prints anything.

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,55 +1,40 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-/**
- * print_all - prints various types of arguments based on the format.
- * @format: A string that specifies the format of the arguments.
- *          'c' for char, 'i' for int, 'f' for float, 's' for string.
- *          Any other characters are ignored.
- *          For 's', if the string is NULL, it prints "(nil)".
- * @...: The arguments to be printed based on the format.
- */
 void print_all(const char * const format, ...) {
-va_list args;
-int i = 0;
-char c;
-char *s;
-va_start(args, format);
-while (format && format[i])
-{
-c = format[i];
-switch (c)
-{
-case 'c':
-printf("%c", va_arg(args, int));
-break;
-case 'i':
-printf("%d", va_arg(args, int));
-break;
-case 'f':
-printf("%f", (float)va_arg(args, double));
-break;
-case 's':
-{
-s = va_arg(args, char *);
-if (s == NULL) {
-printf("(nil)");
+   char c;
+   char *s;
+   int count = 0;
+	va_list args;
+    va_start(args, format);
+    while (format && format[count]) {
+        c = format[count];
+
+        if (c == 'c') {
+            printf("%c", va_arg(args, int));
+        } else if (c == 'i') {
+            printf("%d", va_arg(args, int));
+        } else if (c == 'f') {
+            printf("%f", (float)va_arg(args, double));
+        } else if (c == 's') {
+            s = va_arg(args, char *);
+            if (s == NULL) {
+                printf("(nil)");
+            } else {
+                printf("%s", s);
+            }
+        }
+
+        count++;
+
+        if (format[count]) {
+            if (c == 'c' || c == 'i' || c == 'f' || c == 's') {
+                printf(", ");
+            }
+        }
+    }
+
+    va_end(args);
+    printf("\n");
 }
-else
-{                 
-printf("%s", s);
-}
-}
-break;
-default:
-break;
-}
-if (format[i + 1])
-{
-printf(", ");
-}
-i++;
-}
-va_end(args);
-printf("\n");
-}
+
