bc16b6ea541998146e69620d95b0ea1d563bbe64
AZBLACKPHOENIX <unlimitednigami@gmail.com>
2023-09-21 16:29:34 -0700

    Print text before main executes

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,7 +6,5 @@
 void first(void) __attribute__((constructor));
 void first(void)
 {
-printf("You're beat! and yet, you must allow,\n
-I bore my house upon my back!\n before the main function is executed.");
-printf("\n");
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
