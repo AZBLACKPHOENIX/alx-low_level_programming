c240b28f865420a83e97d3eddffe968cfb3a0445
AZBLACKPHOENIX <unlimitednigami@gmail.com>
2023-09-21 08:29:10 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * print_list - Print all elements of list_s.
+ * @h: Pointer to the head of the list.
+ *
+ * Return: Number of nodes.
+ */
+size_t print_list(const list_t *h)
+{
+    size_t count = 0;
+
+    while (h != NULL)
+    {
+        if (h->str == NULL)
+        {
+            printf("[0] (nil)\n");
+        }
+        else
+        {
+            printf("[%u] %s\n", h->len, h->str);
+        }
+        h = h->next;
+        count++;
+    }
+
+    return count;
+}
