ff4cdc72e24ad474409d3d4406b8070c1d98d9e7
AZBLACKPHOENIX <unlimitednigami@gmail.com>
2023-09-21 17:01:58 -0700

    assmbly code

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,17 @@
+section .data
+    msg db "Hello, Holberton,",10
+
+section .text
+    global main  ; Change _start to main
+
+main:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, msg
+    mov rdx, 18
+    syscall
+
+    mov rax, 60
+    xor rdi, rdi
+    syscall
+
