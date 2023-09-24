6e5c27c1257aa01c2a3b2c01a1031f36a7302daf
AZBLACKPHOENIX <unlimitednigami@gmail.com>
2023-09-24 08:30:35 -0700

    a function that prints all the elements of a listint_t list.

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,12 +11,12 @@
  */
 size_t print_listint(const listint_t *h)
 {
-size_t c = 0;
+size_t count = 0;
 while (h != NULL)
 {
 printf("%u\n", h->n);
+count++;
 h = h->next;
-c++;
 }
-return (c);
+return (count);
 }
