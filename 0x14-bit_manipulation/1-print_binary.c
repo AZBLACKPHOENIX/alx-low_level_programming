fbbf5b8d8dc6154a5f69a2fea8d975a70a1462d5
AZBLACKPHOENIX <unlimitednigami@gmail.com>
2023-10-26 13:28:15 -0700

    printbin

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,28 +2,12 @@
 #include "main.h"
 
 /**
- * print_binary - prints binary represantation of an int
- * @n: value to check
+ * print_binary - Prints the binary representation of a number.
+ * @n: The number to be printed in binary.
  */
 void print_binary(unsigned long int n)
 {
-int i;
-unsigned int mask = 1U << 31;
-int leadingZeros = 1;
-for (i = 0; i < 32; i++)
-{
-if ((n & mask) == 0)
-{
-if (!leadingZeros || i == 3)
-{
-printf("0");
-}
-}
-else
-{
-leadingZeros = 0;
-printf("1");
-}
-mask >>= 1;
-}
+if (n > 1)
+print_binary(n >> 1);
+_putchar((n & 1) + '0');
 }
